710bb82112caa7d3a1d9d86363553fd7e4ab9b96
Brian <brianruiz123@yahoo.com>
2021-01-06 23:55:46 -0600

    initial migrations

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    created = models.DatTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
     
     def __str__(self) -> str:
