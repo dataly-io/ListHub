ec43d6a74e7cc956abe77f98e4213ac734c9d7d6
Brian <brianruiz123@yahoo.com>
2021-01-07 01:01:38 -0600

    change to models, updated amin site

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,24 @@
 # posts/models.py
 from django.db import models
+from django.contrib.auth.models import User
+
+
+STATUS = (
+    (0, 'Draft'), 
+    (1, 'Publish')
+)
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    class Meta:
+        ordering = ['-created']
     
     def __str__(self) -> str:
         return self.title
