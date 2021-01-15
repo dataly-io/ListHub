0c7b0ad9bcade41a74f55d5873aedfa3bbc7f2f8
Brian <brianruiz123@yahoo.com>
2021-01-15 00:24:28 -0600

    updated variable error in model

--- posts/models.py
+++ posts/models.py
@@ -23,9 +23,9 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
-    created = models.DateTimeField(auto_now_add=timezone.now())
-    updated = models.DateField(auto_now=timezone.now())
-    due_date = models.DateField(default=timezone.now()) 
+    created = models.DateTimeField(auto_now_add=timezone.now().strftime('%Y-%m-%d'))
+    updated = models.DateField(auto_now=timezone.now().strftime('%Y-%m-%d'))
+    due_date = models.DateField(default=timezone.now().strftime('%Y-%m-%d'))
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
